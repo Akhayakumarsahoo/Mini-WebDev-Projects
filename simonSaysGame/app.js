743be235fbb69d4b46c5246a8669eac4c0c7fba9
Akhaya <akhayakumarsahoo06@gmail.com>
2024-07-16 18:05:15 +0530

    modified

--- simonSaysGame/app.js
+++ simonSaysGame/app.js
@@ -21,7 +21,7 @@ function levelUp() {
     level++;
     start.innerText = `Level ${level}`;
 
-    let randIdx = Math.floor(Math.random() * 3);
+    let randIdx = Math.floor(Math.random() * 4);
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
     gameSeq.push(randColor);
